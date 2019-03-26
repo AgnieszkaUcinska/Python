4d79b2de87d8bf91e5666c49d64f1bd97882d90e
AgnieszkaUcinska <agnieszka.ucinska81@gmail.com>
2019-03-26 21:34:16 +0100

    Create python2.py

---
+++ python2.py
@@ -0,0 +1,10 @@
+print ("Podaj x")
+x=int(input())
+
+if x > 0 and x<50:
+	print ("X jest mniejszy od 50")
+elif x >=50 and x < 500:
+	print ("x jest większy lub równy  50 ale mniejszy niż 500")
+elif x>=500 and x<=5000:
+	print ("x jest pomiędzy 500 a 5000")
+else: print ("x jest większy niż 5000")
